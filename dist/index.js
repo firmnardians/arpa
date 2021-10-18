4b1bf78921c43a891569e67b0f576173f28620ea
firmnardians <firmnardians@gmail.com>
2021-10-18 13:34:58 +0700

    refactoring code

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.arpa = void 0;
-function arpa(data, limit, page) {
+function arpa({ data, limit, page }) {
     const start = limit * (page - 1);
     const end = limit * page;
-    return {
+    const pagination = {
         *[Symbol.iterator]() {
             for (let i = start; i < data.length && i < end; i++) {
                 yield data[i];
             }
         },
     };
+    return [...pagination];
 }
-exports.arpa = arpa;
+exports.default = arpa;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-export function arpa(data: string[], limit: number, page: number) {
+export default function arpa({ data, limit, page }: { data: string[]; limit: number; page: number }) {
 	const start = limit * (page - 1);
 	const end = limit * page;
 
-	return {
+	const pagination = {
 		*[Symbol.iterator]() {
 			for (let i = start; i < data.length && i < end; i++) {
 				yield data[i];
 			}
 		},
 	};
+
+	return [...pagination];
 }
